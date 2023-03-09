17267b4d6bf9cff1555583d98e1d105743e1a93a
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-09 10:09:29 -0700

    modified code to use inferred typing, ternary operator, and string interpolation

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Given Code
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            // Modified Code
+            var answer = 4; // inferred typing with var instead of int
+            string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; // ternary operator in conjunction with string interpolation
+            Console.WriteLine(response);
+
         }
     }
 }
