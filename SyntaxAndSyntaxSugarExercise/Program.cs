372a570f895442c9bfbd91f9a7832689f7747bfe
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-09 10:10:58 -0700

    changed response from explicit to inferred type

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -18,7 +18,7 @@
 
             // Modified Code
             var answer = 4; // inferred typing with var instead of int
-            string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; // ternary operator in conjunction with string interpolation
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; // ternary operator in conjunction with string interpolation
             Console.WriteLine(response);
 
         }
